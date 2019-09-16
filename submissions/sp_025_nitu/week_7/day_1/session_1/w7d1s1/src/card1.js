b06fa131c6a4d7e35798737c6dae37293bafc194
023nitu <anilamsingh.nitu023@gmail.com>
2019-09-16 21:49:17 +0530

    W7 D1 CS1

---
+++ submissions/sp_025_nitu/week_7/day_1/session_1/w7d1s1/src/card1.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class Mobile extends React.Component{
+    render(){
+        return (
+            <div>
+        <h1>Mobile Operating System</h1>
+        <ul>
+          <li>Android</li>
+          <li>Bloackberry</li>
+          <li>iPhone</li>
+          <li>Window phone</li>
+        </ul>
+        <h1> Mobile Manufacturs</h1>
+        <ul>
+          <li>Samsung</li>
+          <li>HTC</li>
+          <li>Micromax</li>
+          <li>Apple</li>
+        </ul>
+    
+    
+    </div>
+        );
+    }
+}
+
+export default Mobile
