a0b84d52f8c9231616c5103eb59be777f056a210
harshit860 <harshitga@gmail.com>
2019-09-19 20:25:40 +0530

    W7 D1 C1

--- submissions/sp_011_harshit/week_7/day_1/coding_1/src/App.js
+++ submissions/sp_011_harshit/week_7/day_1/coding_1/src/App.js
@@ -3,17 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 import List from './folderA/list'
 import List2 from './folderA/list2' 
-import Fcomp from './folderB/funcompts'
+import Button from './folderB/funcompts'
 import Card from './folderC/card'
-function App() {
+
+class App extends React.Component{
+  
+  constructor(props){
+    super(props);
+    this.state={
+     
+         
+          btn1:["JOIN US","SETTINGS","LOGIN","CONTACT US","SEARCH","HELP","HOME","DOWNLOAD"],
+     
+         colors:["purple ","indigo ","plum", "blue","green ","yellow ","orange ","red "]
+    }
+  }
+render() {
   return (
     <div>
       <List />
       <List2 />
-      <Fcomp />
+        <div className="row mt-5"> 
+          <div className="col-xl-1"><Button items={this.state.btn1[0]} items2={this.state.colors[0]}/></div>
+          <div className="col-xl-1"><Button items={this.state.btn1[1]} items2={this.state.colors[1]}/></div>
+      </div>
+      <div className="row mt-2"> 
+          <div className="col-xl-1"><Button items={this.state.btn1[2]} items2={this.state.colors[2]}/></div>
+          <div className="col-xl-1"><Button items={this.state.btn1[3]} items2={this.state.colors[3]}/></div>
+      </div>
+      <div className="row mt-2"> 
+          <div className="col-xl-1"><Button items={this.state.btn1[4]} items2={this.state.colors[4]}/></div>
+          <div className="col-xl-1"><Button items={this.state.btn1[5]} items2={this.state.colors[5]}/></div>
+      </div>
+      <div className="row mt-2"> 
+          <div className="col-xl-1"><Button items={this.state.btn1[6]} items2={this.state.colors[6]}/></div>
+          <div className="col-xl-1"><Button items={this.state.btn1[7]} items2={this.state.colors[7]}/></div>
+      </div>
+      
+      
       <Card />
     </div>
   );
+}
 }
 
 
