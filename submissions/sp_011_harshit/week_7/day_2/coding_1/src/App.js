562775bfc65be45f98061906f316aef8e34890c3
harshit860 <harshitga@gmail.com>
2019-09-20 20:57:01 +0530

    W7 D2 C1

--- submissions/sp_011_harshit/week_7/day_2/coding_1/src/App.js
+++ submissions/sp_011_harshit/week_7/day_2/coding_1/src/App.js
@@ -5,29 +5,35 @@ import Show1 from './component/Show1'
 import Button from './component/Button'
 import Card from './component/Card'
 
-class App extends React.Component{
-  
-  constructor(props){
+class App extends React.Component {
+
+  constructor(props) {
     super(props);
-    this.state={
-     head1:"Mobile Operating Systems",
-     head2:"Mobile manufacturers",
-      arr1:["Android", "IOS", "Windows Phone", "Symbian"],
+    this.state = {
+      head1: "Mobile Operating Systems",
+      head2: "Mobile manufacturers",
+      arr1: ["Android", "IOS", "Windows Phone", "Symbian"],
       arr2: ["Samsung", "Apple", "Google", "OnePlus"],
-          btn1:["JOIN US","SETTINGS","LOGIN","CONTACT US","SEARCH","HELP","HOME","DOWNLOAD"],
-     name:"Harshit Gakhar",
-     location:"Bangalore",
-     description:"Full stack developer",
-     skills:["html","css","bootstrap","javascript","react"],
-         colors:["purple ","indigo ","plum", "blue","green ","yellow ","orange ","red "]
+      //btn1: ["JOIN US", "SETTINGS", "LOGIN", "CONTACT US", "SEARCH", "HELP", "HOME", "DOWNLOAD"],
+      name: "Harshit Gakhar",
+      location: "Bangalore",
+      description: "Full stack developer",
+      skills: ["html", "css", "bootstrap", "javascript", "react"],
+      //colors: ["purple ", "indigo ", "plum", "blue", "green ", "yellow ", "orange ", "red "],
+      collect: [{ btn: "JOIN US", color: "purple" }, { btn: "SETTINGS", color: "indigo" }, { btn: "LOGIN", color: "plum" }, { btn: "CONTACT US", color: "blue" }, { btn: "SEARCH", color: "green" }, { btn: "HELP", color: "yellow" }, { btn: "HOME", color: "orange" }, { btn: "DOWNLOAD", color: "red" }]
     }
   }
-render() {
-  return (
-    <div>
-      <Show1 items={this.state}/>
-      
-        <div className="row mt-5"> 
+  render() {
+    let ad = this.state.collect.map(a => {
+      return <Button items={a.btn} items2={a.color} />
+    })
+    return (
+      <div>
+        <Show1 items={this.state} />
+        <div className="row w-25">
+          {ad}
+        </div>
+        {/* <div className="row mt-5"> 
           <div className="col-xl-1"><Button items={this.state.btn1[0]} items2={this.state.colors[0]}/></div>
           <div className="col-xl-1"><Button items={this.state.btn1[1]} items2={this.state.colors[1]}/></div>
       </div>
@@ -44,11 +50,11 @@ render() {
           <div className="col-xl-1"><Button items={this.state.btn1[7]} items2={this.state.colors[7]}/></div>
       </div>
       
-      
-      <Card items={this.state}/>
-    </div>
-  );
-}
+       */}
+        <Card items={this.state} />
+      </div>
+    );
+  }
 }
 
 
--- submissions/sp_011_harshit/week_7/day_2/coding_1/src/component/Card.js
+++ submissions/sp_011_harshit/week_7/day_2/coding_1/src/component/Card.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Card.css';
 
-function Card(props){
-    return(
+function Card(props) {
+    return (
         <div className="purple col-xl-3" >
             <img className="rounded-circle offset-2" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLJoUuYsLDgb1dDQLWqNGX3nxK4wMBVVuxvAYLDIDCFl5u1Z92"></img>
             <h3 className="text-white text-center">{props.items.name}</h3>
@@ -11,13 +11,12 @@ function Card(props){
             <div className="row offset-3 col-xl-12  ">
                 <button className="btn w-25 border border-secondary btn text-white  text-dark cyan ">Message</button>
                 <button className="btn w-25 text-info border border-info ml-3">Follow</button>
-
             </div>
             <div className="purple2 col-xl-12 mt-4">
-                    {props.items.skills.map(a=>
-                        <button className="border border-secondary btn text-white col-xl-3 ml-3 mt-3">{a}</button>
+                {props.items.skills.map(a =>
+                    <button className="border border-secondary btn text-white col-xl-3 ml-3 mt-3">{a}</button>
 
-                    )}
+                )}
             </div>
 
 
