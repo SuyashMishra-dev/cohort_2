b1cc0b0947aca22d0cff28469978253d0dc9cbe2
naga <nagasowmyanarayanan@gmail.com>
2019-09-25 15:24:41 +0530

    W8 D3 C1

---
+++ submissions/sp_022_naga-sowmya/week_8/day_3/session_1/trial/src/components/about.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div className="mt-3">
+            <div className="card text-center">
+                <div class="card-header">
+                    Tesla, Inc.
+                </div>
+                <div className="card-body">
+                    <h5 className="card-title">CEO: Elon Musk</h5>
+                    <p className="card-text">Headquarters: Palo Alto, California, U.S.</p>
+                    <a href="#" className="btn btn-primary">More</a>
+                </div>
+                <div className="card-footer text-muted">
+                    NET WORTH: US$21.461 billion
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default About;
