93e937868667a911e3a4c7ba6319afce579ddf36
Nikhil Gudur <gns.nikhil@gmail.com>
2019-09-25 17:00:17 +0530

    W8 D3 C1

---
+++ submissions/sp_023_nikhil/week_8/day_3/session_1/src/components/Contact/Contact.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <>
+            <div className="card offset-4 mt-5" style={{width: "18rem"}}>
+                <div className="card-header">
+                    Contact us at
+                </div>
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">noemail@nomail.com</li>
+                    <li className="list-group-item">Phone No: 001122334455</li>
+                    <li className="list-group-item">Bangalore</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
+export default Contact;
