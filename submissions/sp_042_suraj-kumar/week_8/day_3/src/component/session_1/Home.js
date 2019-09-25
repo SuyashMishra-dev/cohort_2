b13d1ae5ff1df984290bec1434d5f8695e7ad6fc
surrajjha <surrajjha@gmail.com>
2019-09-25 17:09:03 +0530

    W8 D3 C1

---
+++ submissions/sp_042_suraj-kumar/week_8/day_3/src/component/session_1/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Route,BrowserRouter,Link} from 'react-router-dom';
+
+const Home = () => {
+    return <div>
+        <img src="https://www.stlmag.com/downloads/291284/download/0219_Elmwood_0016.jpg?cb=05f56521ae049e15a8f3d244cafb3822&w=640"></img>
+        </div>;
+  };
+
+
+  export default Home;
