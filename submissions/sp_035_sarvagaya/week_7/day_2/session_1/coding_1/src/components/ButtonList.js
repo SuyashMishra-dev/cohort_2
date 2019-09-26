2952b06b116edab9ba8988bbc628af87d67a151e
Sarvagaya <sarvagayaabrol@gmail.com>
2019-09-26 20:28:18 +0530

    W7 D2 C1

---
+++ submissions/sp_035_sarvagaya/week_7/day_2/session_1/coding_1/src/components/ButtonList.js
@@ -0,0 +1,10 @@
+const ButtonList =
+[
+    {
+        id:"A",
+        color:["blue", "yellow", "green", "pink", "teal", "maroon", "purple", "brown"],
+        label:["JOIN US", "LOGIN", "SEARCH", "HOME", "SETTINGS", "CONTACT US", "HELP", "DOWNLOAD"]
+    }
+]
+
+export default ButtonList;
