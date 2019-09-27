5de581eb804c60efec5448a0d7411edfba7c4777
Nikhil Gudur <gns.nikhil@gmail.com>
2019-09-27 14:09:47 +0530

    W7 D3 C1

---
+++ submissions/sp_023_nikhil/week_7/day_3/session_1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Add from './components/Add/Add'
+
+class App extends React.Component{
+  render() {
+    return(
+      <div>
+        <Add />
+      </div>
+    )
+  }
+}
+
+export default App;
