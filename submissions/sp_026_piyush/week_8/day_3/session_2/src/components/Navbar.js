e3354337bba4107a6be2fb5686571b936b05ffcd
Piyush Yadav <work.piyush.yadav@gmail.com>
2019-09-27 12:03:51 +0530

    W8 D3 C2

--- submissions/sp_026_piyush/week_8/day_3/session_2/src/components/Navbar.js
+++ submissions/sp_026_piyush/week_8/day_3/session_2/src/components/Navbar.js
@@ -3,45 +3,39 @@ import { Route, Link } from "react-router-dom";
 import '../App.css'
 import Home from './Home';
 import About from './About';
+import FAQ from './FAQ';
 import Contact from './Contact';
-import Login from './Login';
-import Services from './Services';
-
+import Products from './products/Products';
 
 class Navbar extends Component {
   render() {
     return (
-        // <React.Fragment>
-
-        //   <Link className="btn btn-primary" to="/">Home</Link>
-          
-        //   <Link className="btn btn-primary" to="/about">About</Link>
-
-        //   <Link className="btn btn-primary"to="/contact">Contact</Link>
-
-        //   <Link className="btn btn-primary"to="/services">Services</Link>
-
-        //   <Link className="btn btn-primary"to="/login">Login</Link>
-
-        //   <Route path="/" exact component={Home} />
-
-        //   <Route path="/about" component={About} />
-
-        //   <Route path ="/contact" component={Contact} />
-
-        //   <Route path="/services" component={Services} />
-
-        //   <Route path="/login" component={Login} />
-          
-        // </React.Fragment>
-        
         <React.Fragment>
+                  
+          <nav className="navbar navbar-light bg-light fixed-top">
 
-        <nav className="navbar navbar-light bg-light fixed-top">
-          <Link to="/"><img src="https://cdn.shopify.com/s/files/1/0173/8828/files/Proper_Logo_280x_2x_240x240_6b887461-1064-47c5-a958-b6e5a5726d97_280x@2x.png?v=1532311943"/></Link>
-        </nav>
+          {/* Links */}
+            <Link to="/"><img src="https://cdn.shopify.com/s/files/1/0173/8828/files/Proper_Logo_280x_2x_240x240_6b887461-1064-47c5-a958-b6e5a5726d97_280x@2x.png?v=1532311943"/></Link>
 
-          <Route path="/" component={Home}></Route>
+            <Link className="nav-link" to="/products">Products</Link>
+            <Link className="nav-link" to="/FAQ">FAQ's</Link>
+            <Link className="nav-link" to="/about">About Us</Link>
+            <Link className="nav-link" to="/contact">Contact Us</Link>
+
+            
+            <Link to="#"><img src="https://www.pinclipart.com/picdir/middle/403-4035278_png-file-svg-shopping-cart-icon-png-clipart.png" style={{height: '50px'}} /></Link>
+          </nav>
+          {/* End of Links */}
+
+          {/* Routes */}
+            <Route path="/" exact component={Home}></Route>
+            <Route path="/products" component={Products}></Route>
+            <Route path="/FAQ" component={FAQ}></Route>
+            <Route path="/about" component={About}></Route>
+            <Route path="/contact" component={Contact}></Route>
+
+          {/* End of Routes */}
+
         </React.Fragment>
     );
   }
--- submissions/sp_026_piyush/week_8/day_3/session_2/src/components/Services.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Services = () => {
-    return <div>This is the Services page!</div>
-  };
-
-export default Services;
