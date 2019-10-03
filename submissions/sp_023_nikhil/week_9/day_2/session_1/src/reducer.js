bff0fa1c27a7922b730a7b6f94b7336b85d1c6aa
Nikhil Gudur <gns.nikhil@gmail.com>
2019-10-03 18:11:17 +0530

    W9 D2 C1

--- submissions/sp_023_nikhil/week_9/day_2/session_1/src/reducer.js
+++ submissions/sp_023_nikhil/week_9/day_2/session_1/src/reducer.js
@@ -6,25 +6,31 @@ const MULTIPLY_COUNTER = "MULTIPLY_COUNTER"
 
 const DIVIDE_COUNTER = "DIVIDE_COUNTER"
 
+const COND_INC = "COND_INC"
+
 const initialState = {
-    count: 1
+    count: 0
 }
 
 const counter = (state = initialState, action) => {
     switch (action.type) {
         case INC_COUNTER:
-            if (state.count % 2 !== 0){ 
+            return {
+                count: state.count + action.amount
+            }
+        case COND_INC:
+            if (state.count % 2 !== 0) {
                 return {
-                    count: state.count + action.amount + 1
+                    count: state.count + 1
                 }
             }
-            else if (state.count % 2 === 0){
+            else if (state.count % 2 === 0) {
                 return {
-                    count: state.count + action.amount + 2
+                    count: state.count + 2
                 }
             }
             break;
-            case DEC_COUNTER:
+        case DEC_COUNTER:
             return {
                 count: state.count - action.amount
             }
@@ -33,12 +39,12 @@ const counter = (state = initialState, action) => {
                 count: state.count * action.amount
             }
         case DIVIDE_COUNTER:
-            if (state.count % action.amount === 0){
+            if (state.count % action.amount === 0) {
                 return {
                     count: state.count / action.amount
                 }
             }
-            else if (state.count % action.amount !== 0){
+            else if (state.count % action.amount !== 0) {
                 return {
                     count: state.count % action.amount
                 }
