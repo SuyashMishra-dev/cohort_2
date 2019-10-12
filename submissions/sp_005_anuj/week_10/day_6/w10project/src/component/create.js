eea5cc3a5afa4c712f32a4b9b8bcd2b9d86617b0
choudharyanuj <anuj-choudhary@outlook.com>
2019-10-12 19:12:40 +0530

    Started from Scratch Again

--- submissions/sp_005_anuj/week_10/day_6/w10project/src/component/create.js
+++ submissions/sp_005_anuj/week_10/day_6/w10project/src/component/create.js
@@ -1,24 +1,39 @@
 import React from 'react';
+import { tsExpressionWithTypeArguments } from '@babel/types';
 class Create extends React.Component {
+    constructor (props){
+        super(props)
+        this.state = {
+            name : this.name,
+            country : this.country,
+            t20s : this.t20s,
+            odis : this.odis,
+            tests : this.tests
+        }
+    }
         render() {
         return(
-            <div>
-                <input placeholder = "Enter Name"></input>
-                <select>
-                    <option>India</option>
-                    <option>England</option>
-                    <option>Pakistan</option>
-                    <option>New Zealand</option>
-                    <option>Australia</option>
-                    <option>Zimbabwe</option>
-                    <option>USA</option>
-                    <option>Canada</option>
-                    <option>China</option>
-                    <optin>West indies</optin>
-                </select>
-                <input placeholder = "T20 Score"></input>
-                <input placeholder = "ODI Score"></input>
-                <input placeholder = "Test Score"></input>
+            <div className = "App">
+                <h1>Create Your Team</h1>
+                <div className = "container-fluid"> 
+                    <input placeholder = "Enter Name" style = {{borderRadius: "15px"}}></input><br></br>
+                    <select style = {{borderRadius: "15px"}}> 
+                        <option>India</option>
+                        <option>England</option>
+                        <option>Pakistan</option>
+                        <option>New Zealand</option>
+                        <option>Australia</option>
+                        <option>Zimbabwe</option>
+                        <option>USA</option>
+                        <option>Canada</option>
+                        <option>China</option>
+                        <optin>West indies</optin>
+                    </select><br></br>
+                    <input type = "number" placeholder = "T20 Score" style = {{borderRadius: "15px"}}></input><br></br>
+                    <input type = "number" placeholder = "ODI Score" style = {{borderRadius: "15px"}}></input><br></br>
+                    <input type = "number" placeholder = "Test Score" style = {{borderRadius: "15px"}}></input><br></br>
+                    <button style = {{borderRadius: "15px"}}>Submit</button>
+                </div>
             </div>
         );
     }   
