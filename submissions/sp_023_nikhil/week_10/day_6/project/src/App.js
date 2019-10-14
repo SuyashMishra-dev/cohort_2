a287ede2ead8a98a0a32bf64e05d516e4c3d6bd6
Nikhil Gudur <gns.nikhil@gmail.com>
2019-10-14 15:53:27 +0530

    W10 P created form

---
+++ submissions/sp_023_nikhil/week_10/day_6/project/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Form from './components/From'
+
+
+
+
+class App extends React.Component{
+  render() {
+    return(
+      <>
+        <Form />
+      </>
+    )
+  }
+}
+
+export default App;
