2a2777b06fd68a1b1a60d97e17f6fd335be2f7be
ravipgornal <ravipgornal143@gmail.com>
2019-10-16 18:05:41 +0530

    update

--- submissions/sp_031_ravi/week_10/day_6/cricbuzz/src/components/AllPlayer.js
+++ submissions/sp_031_ravi/week_10/day_6/cricbuzz/src/components/AllPlayer.js
@@ -2,10 +2,30 @@ import React from 'react'
 
 
 class AllPlayer extends React.Component{
+    constructor(){
+        super();
+        let retrieveData = localStorage.getItem("Details");
+        let details = JSON.parse(retrieveData)
+        this.state = {
+            element : details
+    }
+}
     render(){
+       
         return(
-            <h1>Hii...AllPlayers</h1>
+            <div>
+            {this.state.element.map(ele =>{
+                console.log(ele.name)
+                return(
+                    <div key={Math.random()}>
+                        <div>{ele.id}</div>
+                        <div >{ele.name}</div>
+                    </div>
+                )
+            })}
+            </div>
         )
     }
+    
 }
 export default AllPlayer;
