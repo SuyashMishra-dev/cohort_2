0de4d7ad08eac2874ba0e39a6d4e35aea5f91927
naga <nagasowmyanarayanan@gmail.com>
2019-10-25 10:26:51 +0530

    W12 D4 C1

---
+++ submissions/sp_022_naga-sowmya/week_12/day_4/session_1/range_limit.py
@@ -0,0 +1,6 @@
+target = int(input("Enter target: "))
+num = 0
+
+for i in range(target+1):
+    print(num)
+    num = num + 1
---
+++ submissions/sp_022_naga-sowmya/week_12/day_4/session_1/reverse_string.py
@@ -0,0 +1,4 @@
+string = str(input("Enter the string within the quotes: "))
+
+for i in range(len(string)):
+    print(string[len(string)-i-1])
